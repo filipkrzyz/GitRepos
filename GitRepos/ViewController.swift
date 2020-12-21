486d7e4b991ac777170030b8c80e0f244c99256d
filipkrzyz <krzyzanowski.f@gmail.com>
2020-12-21 14:33:25 +0100

    Initial Commit

---
+++ GitRepos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitRepos
+//
+//  Created by Filip Krzyzanowski on 21/12/2020.
+//  Copyright © 2020 Filip Krzyzanowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
